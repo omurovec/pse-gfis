f1a26dac7f7f5d30fe557616f4dbd3c6c91babf7
omurovec <owenmurovec@gmail.com>
2024-01-19 13:53:52 -0800

    feat: use edge chromium

--- pages/api/repo-preview.tsx
+++ pages/api/repo-preview.tsx
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { renderToString } from "react-dom/server";
-import puppeteer from "puppeteer";
 
+import { renderPng } from "./utils";
 import RepoPreview from "../../components/RepoPreview";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -15,20 +15,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     />
   );
 
-  const browser = await puppeteer.launch({
-    headless: true,
-  });
-  const page = await browser.newPage();
-  await page.setViewport({
-    width: 400,
-    height: 70,
-    deviceScaleFactor: 2,
-  });
-  await page.setContent(html);
-  const screenshot = await page.screenshot({
-    omitBackground: true,
-  });
-  await browser.close();
+  const png = await renderPng(html);
 
-  res.status(200).setHeader("Content-Type", "image/png").send(screenshot);
+  res.status(200).setHeader("Content-Type", "image/png").send(png);
 };
