70e63d74a490f9f1815927dedf2a78eadd6ff128
omurovec <owenmurovec@gmail.com>
2024-01-19 15:07:00 -0800

    feat: switch to svg for repo preview

--- pages/api/repo-preview.tsx
+++ pages/api/repo-preview.tsx
@@ -1,21 +1,39 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { renderToString } from "react-dom/server";
 
-import { renderPng } from "./utils";
-import RepoPreview from "../../components/RepoPreview";
+const renderRepoPreview = (repo: { avatarUrl: string; name: string }) => `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        role="img"
+        viewBox="0 0 200 50"
+        version="1.1"
+        width="200"
+        height="50"
+        aria-hidden="true"
+      >
+        <style>
+        * {
+            font-family: -apple-system, "system-ui", "Segoe UI", "Noto Sans", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji";
+        }
+        .title {
+            font-weight: 600;
+            font-size: 1.5rem;
+            fill: white;
+        }
+        </style>
+        <image href="${repo.avatarUrl}" height="30" width="30" x="0" y="0" clip-path="inset(0% round 2px)"/>
+        <text class="title" x="40" y="20">${repo.name}</text>
+    </svg>
+`;
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const html = renderToString(
-    <RepoPreview
-      repo={{
+  res
+    .status(200)
+    .setHeader("Content-Type", "image/svg+xml")
+    .send(
+      renderRepoPreview({
         avatarUrl: "https://avatars.githubusercontent.com/u/90033109?v=4",
         name: "semaphore",
-        url: "any",
-      }}
-    />
-  );
-
-  const png = await renderPng(html);
-
-  res.status(200).setHeader("Content-Type", "image/png").send(png);
+      })
+    );
 };
